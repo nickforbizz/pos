5f8d20f1ff351d4c5c0aee8b4361d54e2fd7a374
Nicholas Waruingi <nwaruingi@kemri-wellcome.org>
2024-03-30 08:02:15 +0300

    Tenant & employee migration

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -1,46 +1,114 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
-
 namespace App\Models;
 
-use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
- * Class Tenant
- * 
- * @property int $id
- * @property string|null $name
- * @property string|null $domain
- * @property string|null $email
- * @property int|null $status
- * @property int|null $active
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property string|null $deleted_at
- *
- * @package App\Models
+ * @property integer $id
+ * @property string $name
+ * @property string $domain
+ * @property string $email
+ * @property integer $status
+ * @property integer $active
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $deleted_at
+ * @property Customer[] $customers
+ * @property EmployeeSalary[] $employeeSalaries
+ * @property Employee[] $employees
+ * @property Expense[] $expenses
+ * @property OrderItem[] $orderItems
+ * @property Order[] $orders
+ * @property ProductCategory[] $productCategories
+ * @property Product[] $products
+ * @property Supplier[] $suppliers
+ * @property User[] $users
  */
 class Tenant extends Model
 {
-	use SoftDeletes, HasFactory;
-	protected $table = 'tenants';
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'domain', 'email', 'status', 'active', 'created_at', 'updated_at', 'deleted_at'];
 
-	protected $casts = [
-		'status' => 'int',
-		'active' => 'int'
-	];
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function customers()
+    {
+        return $this->hasMany('App\Models\Customer', 'fk_tenant');
+    }
 
-	protected $fillable = [
-		'name',
-		'domain',
-		'email',
-		'status',
-		'active'
-	];
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function employeeSalaries()
+    {
+        return $this->hasMany('App\Models\EmployeeSalary', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function employees()
+    {
+        return $this->hasMany('App\Models\Employee', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function expenses()
+    {
+        return $this->hasMany('App\Models\Expense', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orderItems()
+    {
+        return $this->hasMany('App\Models\OrderItem', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+        return $this->hasMany('App\Models\Order', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function productCategories()
+    {
+        return $this->hasMany('App\Models\ProductCategory', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany('App\Models\Product', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function suppliers()
+    {
+        return $this->hasMany('App\Models\Supplier', 'fk_tenant');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function users()
+    {
+        return $this->hasMany('App\Models\User', 'fk_tenant');
+    }
 }
---
+++ database/migrations/2024_03_30_045929_create_tenants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('name', 100)->nullable();
+            $table->string('domain', 100)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->integer('status')->nullable();
+            $table->integer('active')->nullable()->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
