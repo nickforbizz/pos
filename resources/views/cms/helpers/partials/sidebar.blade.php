9f6e99800a956da046db800b85a089864955aff4
Nicholas Waruingi <nwaruingi@kemri-wellcome.org>
2024-03-31 08:43:32 +0300

    Supplier Routes & Navigation

--- resources/views/cms/helpers/partials/sidebar.blade.php
+++ resources/views/cms/helpers/partials/sidebar.blade.php
@@ -76,6 +76,13 @@
 					</a>
 				</li>
 
+				<li class="nav-item @if(Route::is('suppliers.*')) active @endif">
+					<a href="{{ route('suppliers.index') }}">
+						<i class="fas fa-user-tag"></i>
+						<span class="sub-item"> Suppliers </span>
+					</a>
+				</li>
+
 				<li class="nav-item @if(Route::is('products.*') || Route::is('productCategories.*')) active collapsed @else collapse @endif">
 					<a data-toggle="collapse" href="#sidebar_products">
 						<i class="far fa-clone"></i>
