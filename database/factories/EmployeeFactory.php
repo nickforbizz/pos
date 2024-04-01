8ff78e82b71539b4a658120e74b3169ff2312426
Nicholas Waruingi <nwaruingi@kemri-wellcome.org>
2024-04-01 14:27:11 +0300

    Employee Seeder

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Tenant;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AppModels\Supplier>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->word;
+        $users = User::all();
+        $tenants = Tenant::where('active',1)->get();
+
+        return [
+            'name' => $name,
+            'email' => $this->faker->email(),
+            'phone' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'fk_tenant' => $this->faker->randomElement($tenants),
+        ];
+    }
+}
