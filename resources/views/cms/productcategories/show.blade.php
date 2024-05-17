c48bb60e720b2a4d3937c5fad4f8667332c29209
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2024-05-17 12:08:25 +0300

    product category show products

---
+++ resources/views/cms/productcategories/show.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.cms')
+
+@section('content')
+<div class="page-inner">
+    <div class="page-header">
+        <h4 class="page-title"> Product Category </h4>
+        <ul class="breadcrumbs">
+            <li class="nav-home">
+                <a href="#">
+                    <i class="flaticon-home"></i>
+                </a>
+            </li>
+            <li class="separator">
+                <i class="flaticon-right-arrow"></i>
+            </li>
+            <li class="nav-item">
+                <a href="#"> Product Category</a>
+            </li>
+            <li class="separator">
+                <i class="flaticon-right-arrow"></i>
+            </li>
+            <li class="nav-item">
+                <a href="#">Show</a>
+            </li>
+        </ul>
+    </div>
+    <div class="row">
+
+
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h4 class="card-title">List of Available Record(s)</h4>
+                        @can('create product categories')
+                        <a href="{{ route('productCategories.create') }}" class="btn btn-primary btn-round ml-auto">
+                            <i class="flaticon-add mr-2"></i>
+                            Add Row
+                        </a>
+                        @endcan
+                    </div>
+                </div>
+                <div class="card-body">
+
+
+                    <div class="table-responsive">
+                        @include('cms.helpers.partials.feedback')
+                        <table id="tb_productCategories" class="display table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Img</th>
+                                    <th>Title</th>
+                                    <th>Created At</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- .page-inner -->
+
+@endsection
+
+
+@push('scripts')
+
+
+<script>
+    $(document).ready(function() {
+        $('#tb_productCategories').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('productCategories.show', ['productCategory' => $productCategory]) }}",
+            columns: [{
+                    data: 'DT_RowIndex',
+                    name: 'DT_RowIndex'
+                },
+                {
+                    data: 'photo'
+                },
+                {
+                    data: 'title'
+                },
+                {
+                    data: 'created_at',
+                },
+            ]
+        });
+        // #tb_productCategories
+
+
+    });
+</script>
+
+@endpush
