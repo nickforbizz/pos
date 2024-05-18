42396931c29f1abd494037d4a57f1965383a5e7a
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2024-05-18 14:25:16 +0300

    valuelist frontend

---
+++ resources/views/cms/valuelists/index.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.cms')
+
+@section('content')
+<div class="page-inner">
+    <div class="page-header">
+        <h4 class="page-title"> Valuelists </h4>
+        <ul class="breadcrumbs">
+            <li class="nav-home">
+                <a href="#">
+                    <i class="flaticon-home"></i>
+                </a>
+            </li>
+            <li class="separator">
+                <i class="flaticon-right-arrow"></i>
+            </li>
+            <li class="nav-item">
+                <a href="#"> Valuelists</a>
+            </li>
+            <li class="separator">
+                <i class="flaticon-right-arrow"></i>
+            </li>
+            <li class="nav-item">
+                <a href="#">Index</a>
+            </li>
+        </ul>
+    </div>
+    <div class="row">
+   
+
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h4 class="card-title">List of Available Record(s)</h4>
+                        @can('create Valuelists')
+                        <a href="{{ route('valuelists.create') }}" class="btn btn-primary btn-round ml-auto" >
+                            <i class="flaticon-add mr-2"></i>
+                            Add Row
+                        </a> 
+                        @endcan
+                    </div>
+                </div>
+                <div class="card-body">
+                   
+
+                    <div class="table-responsive">
+                        @include('cms.helpers.partials.feedback')
+                        <table id="tb_Valuelists" class="display table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Type</th>
+                                    <th>Value</th>
+                                    <th>Index</th>
+                                    <th>Active</th>
+                                    <th>Created At</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- .page-inner -->
+
+@endsection
+
+
+@push('scripts')
+
+
+<script>
+    $(document).ready(function() {
+        $('#tb_Valuelists').DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: "{{ route('Valuelists.index') }}",
+            columns: [{
+                    data: 'DT_RowIndex',
+                    name: 'DT_RowIndex'
+                },
+                {
+                    data: 'type'
+                },
+                {
+                    data: 'value'
+                },
+                {
+                    data: 'index'
+                },
+                {
+                    data: 'active'
+                },					
+                {
+                    data: 'created_at',
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    orderable: true,
+                    searchable: true
+                },
+            ]
+        });
+        // #tb_Valuelists
+
+      
+    });
+
+
+    
+</script>
+
+@endpush
